5598fedf9c28cef74bf6a7a02cc771ef7691f977
jinwoo <pjw3279@gmail.com>
2022-10-23 02:55:36 +0900

    2week
    Clean up.

---
+++ 2nd_week/2nd_week_project.1/2nd_week_project.1/2nd_week_project.1/소스.cpp
@@ -0,0 +1,9 @@
+/*주교재, pp. 50, 확인문제 3번*/
+#include <stdio.h>
+int main(void)
+{
+	printf("StudentID : %d\n", 2021010530);
+	printf("Name : %s\n", "Park jinwoo");
+	printf("Grade : %c\n", 'A');
+	return 0;
+}
--- 2nd_week/2nd_week_project.1/2nd_week_project.1_code/소스.cpp
+++
@@ -1,9 +0,0 @@
- /*주교재, pp. 50, 확인문제 3번*/
-#include <stdio.h>
-int main(void)
-{
-	printf("StudentID : %d\n", 2021010530);
-	printf("Name : %s\n", "Park jinwoo");
-	printf("Grade : %c\n", 'A');
-	return 0;
-}
