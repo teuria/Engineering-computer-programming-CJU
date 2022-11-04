ec790360efdc0cb9edc58de440fac1af4302f8b9
jinwoo <pjw3279@gmail.com>
2022-11-05 02:19:53 +0900

    Report#1
    Submit.

--- assignment.1/project#1/project.1_assignment/project.1_assignment/소스.cpp
+++ assignment.1/project#1/project.1_assignment/project.1_assignment/소스.cpp
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAXNUM 99
-#define MAXTRYNUM 7
-#define MINTRYNUM 3
-#define YES 1
-#define NO 0
-#define PERCENT 100
+#define MAX_NUM 100																	//Maximum random number up to 100.
+#define MIN_NUM 1																	//Minimum random number at least one.
+#define MAX_TRY_NUM 8																//Maximum number of attempts up to 10.
+#define MIN_TRY_NUM 3																//The minimum number of attempts is 3.
+#define YES 1																		//If want to try again, 1.
+#define NO 0																		//If don't want to try again, 0.
+#define PERCENT 100																	//Represents the probability as a percentage.
 
 int main(void)
 {
-	int randnum = 0;
-	int chance = 0;
-	int num = 0;
-	int play = 1;
-	int ch = 0;	
-	int result;
-	int answer;
-	double rate;
-	double win = 0;
-	double lose = 0;
-	int attempts = 0;
+	int randnum = 0;																//A random number.
+	int chance = 0;																	//Number of attempts.
+	int num = 0;																	//The number I entered.
+	int play = 1;																	//Whether to do it or not.
+	int ch = 0;																		//Whether to play the game or not.
+	int answer;																		//Ask if want to try again.
+	double rate;																	//probability of success.
+	double win = 0;																	//the number of wins.
+	double lose = 0;																//Number of defeats.
+	int attempt = 0;																//The sum of the number of wins and losses.
 	srand(time(0));
-	randnum = (rand() % MAXNUM) + 1;
+	randnum = (rand() % MAX_NUM) + MIN_NUM;											//Specify a random number from 1 to 100
 	srand(time(0));
-	chance = (rand() % MAXTRYNUM) + MINTRYNUM;
+	chance = (rand() % MAX_TRY_NUM) + MIN_TRY_NUM;									//Specify a random number from 3 to 10
 	printf("Remaining opportunity : %d\n", chance);
 	while (play == 1)
 	{
@@ -83,8 +83,8 @@ int main(void)
 		{
 			ch++;
 			win++;
-			attempts = win + lose;
-			printf("\nThat's right.\nYour number of attempts is %d\n", attempts);
+			attempt = win + lose;
+			printf("\nThat's right.\nYour number of attempts is %d\n", attempt);
 			play = 0;
 		}
 	}
